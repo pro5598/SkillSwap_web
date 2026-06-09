11b15b5da99fa4ef8c211ca8441c3fd973155efd
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-06-09 21:01:35 +0545

    user model created

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { UserType } from "../types/user.type";
+
+export interface IUser extends UserType, Document {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserMongoSchema: Schema = new Schema<IUser>(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+  },
+  {
+    timestamps: true, // Automatically manages createdAt and updatedAt
+  },
+);
+
+export const UserModel = mongoose.model<IUser>("User", UserMongoSchema);
