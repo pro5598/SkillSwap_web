e882fa3e47a54f6d296ab805397c28ef693e301b
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-06-09 21:00:26 +0545

    dto created

---
+++ backend/src/dtos/user.dto.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { UserSchema } from "../types/user.type";
+
+export const CreateUserDTO = UserSchema.pick({
+  firstName: true,
+  lastName: true,
+  email: true,
+  username: true,
+  password: true,
+});
+export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
+
+export const LoginUserDTO = UserSchema.pick({
+  email: true,
+  password: true,
+});
+export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
