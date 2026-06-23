14b4adf7c7873f46d6a42f3761471180b8e41d21
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-06-23 21:47:54 +0545

    changes made in route for image upload

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
 import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+import { uploads } from "../middlewares/upload.middleware";
 
-const userRouter = Router();
+export const authRouter = Router();
+export const userRouter = Router();
 const userController = new UserController();
 
-userRouter.post("/register", userController.createUser);
-
-userRouter.post("/login", userController.loginUser);
-
-userRouter.post("/logout", userController.logoutUser);
+authRouter.post("/register", userController.createUser);
+authRouter.post("/login", userController.loginUser);
+authRouter.post("/logout", userController.logoutUser);
 
 userRouter.get("/me", authorizedMiddleware, userController.getCurrentUser);
 
-export default userRouter;
+userRouter.put(
+  "/update",
+  authorizedMiddleware,
+  uploads.single("profileImage"),
+  userController.updateUser,
+);
