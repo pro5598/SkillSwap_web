1af160ab040f2ce8d22550909187b7ff40192b08
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-06-23 21:43:31 +0545

    change made in axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
