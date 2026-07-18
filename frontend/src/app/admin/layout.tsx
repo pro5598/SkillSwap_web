b67a64e89d39d557937817e57904804a4780fa30
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-07-18 12:56:44 +0545

    admin layout created

---
+++ frontend/src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import ProtectedRoute from "@/components/ProtectedRoute";
+import DashboardNavbar from "@/components/DashboardNavbar";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ProtectedRoute>
+      <div className="min-h-screen bg-[#F8F9FE] flex flex-col">
+        <DashboardNavbar />
+        <main className="flex-1 max-w-7xl w-full mx-auto p-4 sm:p-6 lg:p-8">
+          {children}
+        </main>
+      </div>
+    </ProtectedRoute>
+  );
+}
