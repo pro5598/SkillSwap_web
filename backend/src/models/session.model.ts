f5feae525cb50b59565669728246a9fd86e12920
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-07-23 15:27:07 +0545

    session model created

---
+++ backend/src/models/session.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { SessionType } from "../types/session.type";
+
+export interface ISession extends Omit<SessionType, "requesterId" | "providerId">, Document {
+  _id: mongoose.Types.ObjectId;
+  requesterId: mongoose.Types.ObjectId;
+  providerId: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SessionMongoSchema: Schema = new Schema<ISession>(
+  {
+    requesterId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    providerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    scheduledAt: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "declined", "completed", "cancelled"],
+      default: "pending",
+    },
+    meetingDetails: { type: String, maxlength: 1000, trim: true },
+    notes: { type: String, maxlength: 2000, trim: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const SessionModel = mongoose.model<ISession>("Session", SessionMongoSchema);
