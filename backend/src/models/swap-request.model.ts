b74954a2d366540be37d7899c632f6a2eb8a5ee8
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-07-23 15:26:46 +0545

    swap request model created

---
+++ backend/src/models/swap-request.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { SwapRequestType } from "../types/swap-request.type";
+
+export interface ISwapRequest extends Omit<SwapRequestType, "senderId" | "receiverId">, Document {
+  _id: mongoose.Types.ObjectId;
+  senderId: mongoose.Types.ObjectId;
+  receiverId: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SwapRequestMongoSchema: Schema = new Schema<ISwapRequest>(
+  {
+    senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    skillOffered: { type: String, required: true },
+    skillWanted: { type: String, required: true },
+    message: { type: String, maxlength: 1000, trim: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "declined", "cancelled"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const SwapRequestModel = mongoose.model<ISwapRequest>("SwapRequest", SwapRequestMongoSchema);
