f34bc9eb16be0ef9b608f9b80821c92463e44899
Amrit Bhattarai <amritbhattarai481@gmail.com>
2026-07-24 17:32:38 +0545

    changes added

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,15 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*", // Proxy to Backend
+        destination: "http://localhost:5002/api/:path*", // Proxy to Backend
+      },
+      {
+        source: "/uploads/:path*",
+        destination: "http://localhost:5002/uploads/:path*", // Proxy static uploads
+      },
+      {
+        source: "/socket.io/:path*",
+        destination: "http://localhost:5002/socket.io/:path*", // Proxy socket.io
       },
     ];
   },
